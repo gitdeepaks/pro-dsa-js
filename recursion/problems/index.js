1b2707a18c3fd3259f1cc23278f7dba6164947bb
gitdeepaks <deepaksankhyan92@outlook.com>
2024-06-11 17:23:31 +0530

    new commit

---
+++ recursion/problems/index.js
@@ -0,0 +1,38 @@
+// rever a string
+
+// iterative
+// Hello
+
+function reverseString(str) {
+  let rev = "";
+  let len = str.length - 1;
+
+  while (len >= 0) {
+    rev = rev + str[len];
+    len = len - 1;
+  }
+  return rev;
+}
+
+// console.log(reverseString("hello"));
+
+function recursiveReverseString(str) {
+  console.log(`current : ${str}`); //winding
+  // Base case
+  if (str === "") {
+    return str;
+  }
+
+  let revPart = recursiveReverseString(str.substring(0, str.length - 1));
+  console.log(revPart);
+
+  let res = str[str.length - 1] + revPart;
+  console.log(
+    `Reversing: Last chat ${
+      str[str.length - 1]
+    } added in front of ${revPart} form res: ${res}`
+  );
+  return res;
+}
+
+console.log(recursiveReverseString("hello"));

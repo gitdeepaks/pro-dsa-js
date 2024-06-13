444ce9aa05c7de38b54fa3e93b1366876047b2e1
gitdeepaks <deepaksankhyan92@outlook.com>
2024-06-13 22:35:49 +0530

    new commit

---
+++ Graphs/Problems/undirToDir.js
@@ -0,0 +1,15 @@
+const createGraph = (edges) => {
+  const graph = {};
+
+  for (const edge of edges) {
+    const [a, b] = edge;
+    if (!(a in graph)) {
+      graph[a] = [];
+    }
+    if (!(b in graph)) {
+      graph[b] = [];
+    }
+    graph[a].push(b);
+    graph[b].push(a);
+  }
+};

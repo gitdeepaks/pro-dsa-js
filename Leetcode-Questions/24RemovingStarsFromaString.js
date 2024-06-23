c2450e18d2c4fb588ce2c2d6d0daa990c9dc682e
gitdeepaks <deepaksankhyan92@outlook.com>
2024-06-23 20:21:43 +0530

    new commit

---
+++ Leetcode-Questions/24RemovingStarsFromaString.js
@@ -0,0 +1,13 @@
+const removeStars = function (s) {
+  let stack = [];
+
+  for (const char of s) {
+    if (char === "*") {
+      stack.pop();
+    } else {
+      stack.push(char);
+    }
+  }
+
+  return stack.join("");
+};

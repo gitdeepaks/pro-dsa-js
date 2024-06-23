22cf14197a4b4c02c2f207d3f5554132ea2f756b
gitdeepaks <deepaksankhyan92@outlook.com>
2024-06-23 22:37:00 +0530

    new commit

--- Leetcode-Questions/26DecodeString.js
+++ Leetcode-Questions/26DecodeString.js
@@ -1,1 +1,27 @@
-var decodeString = function (s) {};
+const decodeString = function (s) {
+  let stack = [];
+  let currentNum = 0;
+  let currentStr = "";
+
+  for (const char of s) {
+    if (char >= "0" && char <= "9") {
+      currentNum = currentNum * 10 + (char - "0");
+    } else if (char === "[") {
+      stack.push(currentNum);
+      stack.push(currentStr);
+
+      //   reset
+      currentNum = 0;
+      currentStr = "";
+    } else if (char === "]") {
+      let prevStr = stack.pop();
+      let repeatTime = stack.pop();
+
+      currentStr = prevStr + currentStr.repeat(repeatTime);
+    } else {
+      currentStr += char;
+    }
+  }
+
+  return currentStr;
+};

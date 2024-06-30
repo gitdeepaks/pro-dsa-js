f4f3b83a8938c92a0134852da8925b0f00373285
gitdeepaks <deepaksankhyan92@outlook.com>
2024-06-30 11:27:13 +0530

    new commit

--- 01-JS-Foundation/debounceEffect/chaiaurcode.js
+++ 01-JS-Foundation/debounceEffect/chaiaurcode.js
@@ -1,62 +1,93 @@
-// Map
+// // Map
 
-const number = [1, 2, 3, 4, 5];
+// const number = [1, 2, 3, 4, 5];
 
-const mul = number.map((num, index, arr) => num * 3 + index);
+// const mul = number.map((num, index, arr) => num * 3 + index);
 
-console.log(mul);
+// console.log(mul);
 
-// filter
-const moreThanTw0 = number.filter((num) => num > 2);
-console.log(moreThanTw0);
+// // filter
+// const moreThanTw0 = number.filter((num) => num > 2);
+// console.log(moreThanTw0);
 
-// Reduce
-const sumOfAllElement = number.reduce((acc, currVal, i, arr) => {
-  return acc + currVal;
-}, 0);
+// // Reduce
+// const sumOfAllElement = number.reduce((acc, currVal, i, arr) => {
+//   return acc + currVal;
+// }, 0);
 
-console.log(sumOfAllElement);
+// console.log(sumOfAllElement);
 
-// Polyfills
+// // Polyfills
 
-// Array.map((num, i, arr) => {});
+// // Array.map((num, i, arr) => {});
 
-Array.prototype.myMap = function (cb) {
-  let temp = [];
-  for (let i = 0; i < this.length; i++) {
-    temp.push(cb(this[i], i, this));
-  }
-  return temp;
-};
-
-const mulMyMap = number.myMap((num, index, arr) => num * 2 + index);
-console.log(mulMyMap);
-
-// custom filter
-
-// Array.prototype.myFilter = function (cb) {
+// Array.prototype.myMap = function (cb) {
 //   let temp = [];
 //   for (let i = 0; i < this.length; i++) {
-//     if (cb(this[i], i, this)) this.push(this[i]);
+//     temp.push(cb(this[i], i, this));
 //   }
 //   return temp;
 // };
 
-// const moreThanTw0Custom = number.myFilter((num) => num > 2);
-// console.log(moreThanTw0Custom);
+// const mulMyMap = number.myMap((num, index, arr) => num * 2 + index);
+// console.log(mulMyMap);
 
-Array.prototype.myReduce = function (cb, initVal) {
-  let acc = initVal;
+// // custom filter
 
-  for (let i = 0; i < this.length; i++) {
-    acc = acc ? cb(acc, this[i], i, this) : this[i];
-  }
+// // Array.prototype.myFilter = function (cb) {
+// //   let temp = [];
+// //   for (let i = 0; i < this.length; i++) {
+// //     if (cb(this[i], i, this)) this.push(this[i]);
+// //   }
+// //   return temp;
+// // };
 
-  return acc;
-};
+// // const moreThanTw0Custom = number.myFilter((num) => num > 2);
+// // console.log(moreThanTw0Custom);
 
-const sumOfAllElementC = number.myReduce((acc, currVal, i, arr) => {
-  return acc + currVal;
-}, 0);
+// Array.prototype.myReduce = function (cb, initVal) {
+//   let acc = initVal;
 
-console.log(sumOfAllElementC);
+//   for (let i = 0; i < this.length; i++) {
+//     acc = acc ? cb(acc, this[i], i, this) : this[i];
+//   }
+
+//   return acc;
+// };
+
+// const sumOfAllElementC = number.myReduce((acc, currVal, i, arr) => {
+//   return acc + currVal;
+// }, 0);
+
+// console.log(sumOfAllElementC);
+
+let students = [
+  {
+    name: "Deepak",
+    rollNumber: 11,
+    marks: 95,
+  },
+  {
+    name: "Jinny",
+    rollNumber: 19,
+    marks: 98,
+  },
+  {
+    name: "Jonny",
+    rollNumber: 20,
+    marks: 95,
+  },
+  {
+    name: "John",
+    rollNumber: 21,
+    marks: 99,
+  },
+];
+
+let names = [];
+
+for (let i = 0; i < students.length; i++) {
+  names.push(students[i].name.toUpperCase());
+}
+
+console.log(names);

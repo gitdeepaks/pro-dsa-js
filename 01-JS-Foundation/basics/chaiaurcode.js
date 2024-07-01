4aaea0ad480bacc944896bc50f1bbed866a6f444
gitdeepaks <deepaksankhyan92@outlook.com>
2024-07-01 09:37:13 +0530

    new commit

--- 01-JS-Foundation/basics/chaiaurcode.js
+++ 01-JS-Foundation/basics/chaiaurcode.js
@@ -1,143 +1,143 @@
-// // Map
+// // // Map
 
-// const number = [1, 2, 3, 4, 5];
+// // const number = [1, 2, 3, 4, 5];
 
-// const mul = number.map((num, index, arr) => num * 3 + index);
+// // const mul = number.map((num, index, arr) => num * 3 + index);
 
-// console.log(mul);
+// // console.log(mul);
 
-// // filter
-// const moreThanTw0 = number.filter((num) => num > 2);
-// console.log(moreThanTw0);
+// // // filter
+// // const moreThanTw0 = number.filter((num) => num > 2);
+// // console.log(moreThanTw0);
 
-// // Reduce
-// const sumOfAllElement = number.reduce((acc, currVal, i, arr) => {
-//   return acc + currVal;
-// }, 0);
+// // // Reduce
+// // const sumOfAllElement = number.reduce((acc, currVal, i, arr) => {
+// //   return acc + currVal;
+// // }, 0);
 
-// console.log(sumOfAllElement);
+// // console.log(sumOfAllElement);
 
-// // Polyfills
+// // // Polyfills
 
-// // Array.map((num, i, arr) => {});
+// // // Array.map((num, i, arr) => {});
 
-// Array.prototype.myMap = function (cb) {
-//   let temp = [];
-//   for (let i = 0; i < this.length; i++) {
-//     temp.push(cb(this[i], i, this));
-//   }
-//   return temp;
-// };
-
-// const mulMyMap = number.myMap((num, index, arr) => num * 2 + index);
-// console.log(mulMyMap);
-
-// // custom filter
-
-// // Array.prototype.myFilter = function (cb) {
+// // Array.prototype.myMap = function (cb) {
 // //   let temp = [];
 // //   for (let i = 0; i < this.length; i++) {
-// //     if (cb(this[i], i, this)) this.push(this[i]);
+// //     temp.push(cb(this[i], i, this));
 // //   }
 // //   return temp;
 // // };
 
-// // const moreThanTw0Custom = number.myFilter((num) => num > 2);
-// // console.log(moreThanTw0Custom);
+// // const mulMyMap = number.myMap((num, index, arr) => num * 2 + index);
+// // console.log(mulMyMap);
 
-// Array.prototype.myReduce = function (cb, initVal) {
-//   let acc = initVal;
+// // // custom filter
 
-//   for (let i = 0; i < this.length; i++) {
-//     acc = acc ? cb(acc, this[i], i, this) : this[i];
-//   }
+// // // Array.prototype.myFilter = function (cb) {
+// // //   let temp = [];
+// // //   for (let i = 0; i < this.length; i++) {
+// // //     if (cb(this[i], i, this)) this.push(this[i]);
+// // //   }
+// // //   return temp;
+// // // };
 
-//   return acc;
-// };
+// // // const moreThanTw0Custom = number.myFilter((num) => num > 2);
+// // // console.log(moreThanTw0Custom);
 
-// const sumOfAllElementC = number.myReduce((acc, currVal, i, arr) => {
-//   return acc + currVal;
-// }, 0);
+// // Array.prototype.myReduce = function (cb, initVal) {
+// //   let acc = initVal;
 
-// console.log(sumOfAllElementC);
+// //   for (let i = 0; i < this.length; i++) {
+// //     acc = acc ? cb(acc, this[i], i, this) : this[i];
+// //   }
 
-let students = [
-  {
-    name: "Deepak",
-    rollNumber: 11,
-    marks: 95,
-  },
-  {
-    name: "Jinny",
-    rollNumber: 19,
-    marks: 98,
-  },
-  {
-    name: "Jonny",
-    rollNumber: 20,
-    marks: 95,
-  },
-  {
-    name: "John",
-    rollNumber: 21,
-    marks: 99,
-  },
-];
+// //   return acc;
+// // };
 
-// Q1 - Return only the names of students in capital
+// // const sumOfAllElementC = number.myReduce((acc, currVal, i, arr) => {
+// //   return acc + currVal;
+// // }, 0);
 
-// Solution 1 : Traditional for() loop
-let names = [];
-for (let index = 0; index < studentRecords.length; index++) {
-  names.push(students[index].name.toUpperCase());
-}
+// // console.log(sumOfAllElementC);
 
-console.log(names);
+// let students = [
+//   {
+//     name: "Deepak",
+//     rollNumber: 11,
+//     marks: 95,
+//   },
+//   {
+//     name: "Jinny",
+//     rollNumber: 19,
+//     marks: 98,
+//   },
+//   {
+//     name: "Jonny",
+//     rollNumber: 20,
+//     marks: 95,
+//   },
+//   {
+//     name: "John",
+//     rollNumber: 21,
+//     marks: 99,
+//   },
+// ];
 
-// Solution 2 : forEach()
-let names = [];
-students.forEach((student) => {
-  names.push(student.name.toUpperCase());
-});
+// // Q1 - Return only the names of students in capital
 
-console.log(names);
+// // Solution 1 : Traditional for() loop
+// let names = [];
+// for (let index = 0; index < studentRecords.length; index++) {
+//   names.push(students[index].name.toUpperCase());
+// }
 
-// Solution 3 : map()
-let names = students.map((stu) => stu.name.toUpperCase());
-console.log(names);
+// console.log(names);
 
-// Q2 - we want to get the details of students who scored more than 60 marks.
+// // Solution 2 : forEach()
+// let names = [];
+// students.forEach((student) => {
+//   names.push(student.name.toUpperCase());
+// });
 
-let names = students.filter((stu) => stu.marks > 60);
-console.log(names);
+// console.log(names);
 
-// Q3 - Get the details of students who scored more than 60 marks and have rollNumber greater than 15.
+// // Solution 3 : map()
+// let names = students.map((stu) => stu.name.toUpperCase());
+// console.log(names);
 
-let names = students.filter((stu) => stu.marks > 60 && stu.rollNumber > 20);
-console.log(names);
+// // Q2 - we want to get the details of students who scored more than 60 marks.
 
-// Q4 - Sum total of the marks of the students
+// let names = students.filter((stu) => stu.marks > 60);
+// console.log(names);
 
-let totalMarks = students.reduce((acc, emp) => acc + emp.marks, 0);
-console.log(totalMarks);
+// // Q3 - Get the details of students who scored more than 60 marks and have rollNumber greater than 15.
 
-// Q5 - Get only the names of the students who scored more than 60 marks
+// let names = students.filter((stu) => stu.marks > 60 && stu.rollNumber > 20);
+// console.log(names);
 
-let names = students.filter((stu) => stu.marks > 50).map((stu) => stu.name);
-console.log(names);
+// // Q4 - Sum total of the marks of the students
 
-// Q6 - print the total marks of the students with marks greater than 60 after 20 marks has been added to those students who scored less than 60.
+// let totalMarks = students.reduce((acc, emp) => acc + emp.marks, 0);
+// console.log(totalMarks);
 
-let totalMarkss = studentRecords
-  .map(function (stu) {
-    if (stu.marks < 60) {
-      stu.marks += 20;
-    }
-    return stu;
-  })
-  .filter((stu) => stu.marks > 60)
-  .reduce((acc, curr) => acc + curr.marks, 0);
+// // Q5 - Get only the names of the students who scored more than 60 marks
 
-console.log(totalMarks);
+// let names = students.filter((stu) => stu.marks > 50).map((stu) => stu.name);
+// console.log(names);
 
-console.log(names);
+// // Q6 - print the total marks of the students with marks greater than 60 after 20 marks has been added to those students who scored less than 60.
+
+// let totalMarkss = studentRecords
+//   .map(function (stu) {
+//     if (stu.marks < 60) {
+//       stu.marks += 20;
+//     }
+//     return stu;
+//   })
+//   .filter((stu) => stu.marks > 60)
+//   .reduce((acc, curr) => acc + curr.marks, 0);
+
+// console.log(totalMarks);
+
+// console.log(names);
